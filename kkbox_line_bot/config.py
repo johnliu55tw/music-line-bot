7fa36ca88c65cd9409d2b6835545bfb7f133c5a6
John Liu <johnliu@kkbox.com>
2018-04-30 18:00:19 +0800

    Added KKBOX_ACCESS_TOKEN to the configuration.

--- kkbox_line_bot/config.py
+++ kkbox_line_bot/config.py
@@ -14,11 +14,13 @@ def load_default():
     return {'LINE_CHANNEL_ACCESS_TOKEN': 'Line access token for testing',
             'LINE_CHANNEL_SECRET': 'Line channel secret fro testing',
             'OLAMI_APP_KEY': 'Olami service app key',
-            'OLAMI_APP_SECRET': 'Olami app secret'}
+            'OLAMI_APP_SECRET': 'Olami app secret',
+            'KKBOX_ACCESS_TOKEN': 'KKBOX access token'}
 
 
 def load_production():
     return {'LINE_CHANNEL_ACCESS_TOKEN': getenv_raise('LINE_CHANNEL_ACCESS_TOKEN'),
             'LINE_CHANNEL_SECRET': getenv_raise('LINE_CHANNEL_SECRET'),
             'OLAMI_APP_KEY': getenv_raise('OLAMI_APP_KEY'),
-            'OLAMI_APP_SECRET': getenv_raise('OLAMI_APP_SECRET')}
+            'OLAMI_APP_SECRET': getenv_raise('OLAMI_APP_SECRET'),
+            'KKBOX_ACCESS_TOKEN': getenv_raise('KKBOX_ACCESS_TOKEN')}
